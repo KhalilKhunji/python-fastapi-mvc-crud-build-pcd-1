2486f4bab1d60c203eebf45cc1a42cde30361e6d
Khalil Khunji <khalilkhunji@gmail.com>
2025-04-16 16:49:35 +0300

    added some auth

--- controllers/teas.py
+++ controllers/teas.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from models.tea import TeaModel
+from models.user import UserModel
 from serializers.tea import TeaSchema
 from typing import List
 from database import get_db
+from dependencies.get_current_user import get_current_user
 
 router = APIRouter()
 
@@ -20,7 +22,7 @@ def get_single_tea(tea_id: int, db: Session = Depends(get_db)):
     return tea
 
 @router.post("/teas", response_model=TeaSchema)
-def create_tea(tea: TeaSchema, db: Session = Depends(get_db)):
+def create_tea(tea: TeaSchema, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     new_tea = TeaModel(**tea.dict()) # Convert Pydantic model to SQLAlchemy model
     db.add(new_tea)
     db.commit() # Save to database
@@ -28,7 +30,7 @@ def create_tea(tea: TeaSchema, db: Session = Depends(get_db)):
     return new_tea
 
 @router.put("/teas/{tea_id}", response_model=TeaSchema)
-def update_tea(tea_id: int, tea: TeaSchema, db: Session = Depends(get_db)):
+def update_tea(tea_id: int, tea: TeaSchema, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     db_tea = db.query(TeaModel).filter(TeaModel.id == tea_id).first()
     if not db_tea:
         raise HTTPException(status_code=404, detail="Tea not found")
@@ -42,7 +44,7 @@ def update_tea(tea_id: int, tea: TeaSchema, db: Session = Depends(get_db)):
     return db_tea
 
 @router.delete("/teas/{tea_id}")
-def delete_tea(tea_id: int, db: Session = Depends(get_db)):
+def delete_tea(tea_id: int, db: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     db_tea = db.query(TeaModel).filter(TeaModel.id == tea_id).first()
     if not db_tea:
         raise HTTPException(status_code=404, detail="Tea not found")
